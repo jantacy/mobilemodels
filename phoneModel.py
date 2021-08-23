5e6ab6469f66bd6bc10943b5ef3425eb35991d2b
jantacy <20486879+jantacy@users.noreply.github.com>
2021-08-23 12:00:13 +0800

    fix blank

--- phoneModel.py
+++ phoneModel.py
@@ -29,10 +29,10 @@ class PhoneModel:
         model_df=pd.DataFrame(columns=['brand','model','area','brand_name','model_name'])
         for record in brand_info_list:
             record_list=record.replace('`','').split(':')
-            for model in record_list[0].split(' '):
-                if brand[:-3].split('_')[0].lower() == model.lower():
-                    continue
-                model_df.loc[len(model_df)]=(brand[:-3].split('_')[0],model,'en' if brand.find('_en')>0 else 'cn',brand_map.get(brand[:-3].split('_')[0],'其他'),record_list[1])
+            model=record_list[0]
+            if record_list[1]='**':
+                continue
+            model_df.loc[len(model_df)]=(brand[:-3].split('_')[0],model,'en' if brand.find('_en')>0 else 'cn',brand_map.get(brand[:-3].split('_')[0],'其他'),record_list[1])
 
         return model_df
 
